1f90b3a91940f0d89b7099548dbbc0702f76af31
Muhammed Çağrı Kurt <muhammedcagrikurt@gmail.com>
2023-11-21 02:58:03 +0300

    .

--- app/src/main/java/com/kurt/amazonapp/ui/fragment/AnasayfaFragment.java
+++ app/src/main/java/com/kurt/amazonapp/ui/fragment/AnasayfaFragment.java
@@ -24,75 +24,47 @@ public class AnasayfaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false);
-
-
         binding.kategoriRV.setLayoutManager(new LinearLayoutManager(requireContext()));
-        //binding.kategoriRV.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
         ArrayList<Kategori> kategoriArrayList = new ArrayList<>();
         ArrayList<CardResim> cardResimArrayList = new ArrayList<>();
         ArrayList<CardResim> cardResimArrayList2 = new ArrayList<>();
         ArrayList<CardResim> cardResimArrayList3 = new ArrayList<>();
-        CardResim f1 = new CardResim(1,"muthiskasimgulumse"); //dosya ismi yeterli uzantıya gerek yok
-        CardResim f2 = new CardResim(2,"interstellar");
-        CardResim f3 = new CardResim(3,"inception");
-        CardResim f4 = new CardResim(4,"thehatefuleight");
-        CardResim f5 = new CardResim(5,"thepianist");
-        CardResim f6 = new CardResim(6,"anadoluda");
-        cardResimArrayList.add(f1);
-        cardResimArrayList.add(f2);
-        cardResimArrayList.add(f3);
-        cardResimArrayList.add(f4);
-        cardResimArrayList.add(f5);
-        cardResimArrayList.add(f6);
-        cardResimArrayList.add(f1);
+        ArrayList<CardResim> cardResimArrayList4 = new ArrayList<>();
+        CardResim c1 = new CardResim(1,"muthiskasimgulumse"); //dosya ismi yeterli uzantıya gerek yok
+        //CardResim c2 = new CardResim(1,"muthiskasimgulumse");
+        cardResimArrayList.add(c1);
+        //cardResimArrayList.add(c2);
         //
-        cardResimArrayList2.add(f2);
-        cardResimArrayList2.add(f3);
-        cardResimArrayList2.add(f4);
-        cardResimArrayList2.add(f5);
-        cardResimArrayList2.add(f6);
+        CardResim cr1 = new CardResim(1,"gulumsetenkasim");
+        CardResim cr2 = new CardResim(2,"gulumsetenkasim");
+        CardResim cr3 = new CardResim(3,"kitapkategori");
+        cardResimArrayList2.add(cr1);
+        cardResimArrayList2.add(cr2);
+        cardResimArrayList2.add(cr3);
         //
-        cardResimArrayList3.add(f1);
-        cardResimArrayList3.add(f2);
-        cardResimArrayList3.add(f3);
-        cardResimArrayList3.add(f4);
-        cardResimArrayList3.add(f5);
-        cardResimArrayList3.add(f6);
-
+        CardResim cresim1 = new CardResim(1,"uspa");
+        CardResim cresim2 = new CardResim(2,"kedi");
+        CardResim cresim3 = new CardResim(4,"samsung");
+        CardResim cresim4 = new CardResim(5,"dockers");
+        cardResimArrayList3.add(cresim1);
+        cardResimArrayList3.add(cresim2);
+        cardResimArrayList3.add(cresim3);
+        cardResimArrayList3.add(cresim4);
+        //
+        CardResim cardresim1 = new CardResim(1,"puma"); //dosya ismi yeterli uzantıya gerek yok
+        cardResimArrayList4.add(cardresim1);
+        //
         Kategori k1 = new Kategori(1,cardResimArrayList);
         Kategori k2 = new Kategori(2,cardResimArrayList2);
         Kategori k3 = new Kategori(3,cardResimArrayList3);
+        Kategori k4 = new Kategori(4,cardResimArrayList4);
         kategoriArrayList.add(k1);
         kategoriArrayList.add(k2);
         kategoriArrayList.add(k3);
+        kategoriArrayList.add(k4);
 
-        //CardAdapter cardAdapter = new CardAdapter(cardResimArrayList,this);
         YatayRecyclerViewAdapter yatayRecyclerViewAdapter = new YatayRecyclerViewAdapter(kategoriArrayList,requireContext());
         binding.kategoriRV.setAdapter(yatayRecyclerViewAdapter);
-        //binding.kategoriRV.setAdapter(cardAdapter);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         return binding.getRoot();
     }
